a5ee4720dccac2709dd88e10c8fcd6f78129ea76
amshel <amshelhack3r@gmail.com>
2020-03-03 01:02:43 +0000

    add styling to utils class

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -22,3 +22,33 @@ TextStyle getCustomFontTextStyle() {
       // set the font size
       fontSize: 36.0);
 }
+
+//styling
+TextStyle namingHeaders() {
+  return TextStyle(
+      color: Colors.blueAccent,
+      fontFamily: "Nunito",
+      fontWeight: FontWeight.bold,
+      fontSize: 30);
+}
+
+TextStyle labels() {
+  return TextStyle(
+      color: Colors.black87, fontSize: 20, fontFamily: ".SF UI Text");
+}
+
+InputDecoration textFields(IconData data, String hint) {
+  return InputDecoration(
+      prefixIcon: Icon(
+        data,
+        color: Colors.black26,
+      ),
+      hintText: hint,
+      hintStyle: TextStyle(color: Colors.black26),
+      filled: true,
+      fillColor: Colors.white,
+      border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(40.0)),
+      contentPadding: EdgeInsets.fromLTRB(20.0, 35.0, 20.0, 35.0));
+}
